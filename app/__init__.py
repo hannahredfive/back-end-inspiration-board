87d7b75a1cdc36d761f2b6ad186d5bdaef47d624
Sel <selenetenorio27@hotmail.com>
2023-06-28 11:40:05 -0700

    Card routes deleted and SLACK token fixed

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
