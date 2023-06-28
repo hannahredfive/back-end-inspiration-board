13f3e9a9dcf9545a3506e4925ef95595332dbcfd
Sel <selenetenorio27@hotmail.com>
2023-06-28 10:30:31 -0700

    Bug fixed in alembic table

--- app/models/board.py
+++ app/models/board.py
@@ -8,12 +8,15 @@ class Board(db.Model):
     cards = db.relationship("Card", back_populates="board")
 
     def to_dict(self):
-        board_as_dict = {}
-        board_as_dict["id"] = self.board_id
-        board_as_dict["title"] = self.title
-        board_as_dict["owner"] = self.owner
-        
-        return board_as_dict
+        board_dict = {
+            "board_id" : self.board_id,
+            "owner": self.owner,
+            "title": self.title,
+        }
+        if self.cards:
+            board_dict["cards"] = [card.to_dict() for card in self.cards]
+
+        return board_dict
     
     @classmethod
     def from_dict(cls, board_data):
